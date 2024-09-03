0430cf8621cbe5d43ec1924daf467d5bd78212e3
thiru0211 <146463022+thiru0211@users.noreply.github.com>
2024-09-03 17:02:44 +0530

    Update CheckPayments.java

--- src/test/java/allPages/CheckPayments.java
+++ src/test/java/allPages/CheckPayments.java
@@ -23,6 +23,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.openqa.selenium.Keys;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
